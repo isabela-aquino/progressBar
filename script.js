74d613d8a7e30f5f1784498cc33bd40a4bb5b4e8
Isabela Aquino <isabelaaquinom@outlook.com>
2022-03-14 21:26:58 -0300

    Update Pagamento

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
 const login = document.getElementById('container-account');
+const creditCard = document.getElementById('credit-card');
+const boleto = document.getElementById('boleto-payment');
+const paypal = document.getElementById('paypal-payment');
+
 
 const containerAccount = document.getElementById('container-account');
 const containerAddress = document.getElementById('address-container');
 const containerPayment = document.getElementById('payment-container');
 const containerConfirmation = document.getElementById('confirmation-container');
+const containerCreditCard = document.getElementById('option-creditCard');
+const containerBoleto = document.getElementById('option-boleto');
+
+
 
 activeStep = 1;
 
 next.addEventListener('click', onNextClick);
 previous.addEventListener('click', onPreviousClick);
+boleto.addEventListener('click', onBoletoClick);
+creditCard.addEventListener('click', onCreditCardClick);
+paypal.addEventListener('click', onPayPalClick);
+containerBoleto.style.display = 'none';
 hideOrShowTextDiv(1);
+
+
 
 function onNextClick(){
     activeStep += 1;
@@ -25,6 +39,30 @@ function onPreviousClick(){
     hideOrShowTextDiv(activeStep);
 }
 
+function onBoletoClick(){
+    boleto.classList.add('active');
+    creditCard.classList.remove('active');
+    paypal.classList.remove('active');
+    containerCreditCard.style.display = 'none';
+    containerBoleto.style.display = 'block';
+}
+
+function onCreditCardClick(){
+    creditCard.classList.add('active');
+    boleto.classList.remove('active');
+    paypal.classList.remove('active');
+    containerCreditCard.style.display = 'flex';
+    containerBoleto.style.display = 'none';
+}
+
+function onPayPalClick(){
+    creditCard.classList.remove('active');
+    boleto.classList.remove('active');
+    paypal.classList.add('active');
+    containerCreditCard.style.display = 'none';
+    containerBoleto.style.display = 'none';
+}
+
 function hideOrShowTextDiv(activeStep){
     hideTextElements();
     if (activeStep === 1){
@@ -36,7 +74,9 @@ function hideOrShowTextDiv(activeStep){
     }
 
     if (activeStep === 3){
-        containerPayment.style.display ='flex'
+        containerPayment.style.display ='flex';
+        
+        //onBoletoClick();
     }
 
     if (activeStep === 4){
